a701416ce3879bbcf01a17e2bee08cf72c8e0406
Sebastian Jabłoński <Sebastian.Jablonski@imgw.pl>
2022-01-04 20:39:14 +0100

    init

---
+++ src/main/java/com/business/rest/BackendBussinessTasksApplication.java
@@ -0,0 +1,13 @@
+package com.business.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBussinessTasksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendBussinessTasksApplication.class, args);
+    }
+
+}
